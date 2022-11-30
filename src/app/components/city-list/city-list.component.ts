bcf2ca63485ca6de23cd2a8f854dc10418d55a9d
Thurinum <m.gagnon941@gmail.com>
2022-11-29 21:46:45 -0500

    style: Display cities using card layout

--- src/app/components/city-list/city-list.component.ts
+++ src/app/components/city-list/city-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { City } from 'src/app/models/city.model';
+import { SlideAnimation } from 'src/app/animations.module';
 
 @Component({
 	selector: 'app-city-list',
 	templateUrl: './city-list.component.html',
-	styleUrls: ['./city-list.component.sass']
+	styleUrls: ['./city-list.component.sass'],
+	animations: [SlideAnimation]
 })
 export class CityListComponent {
 	@Input() cities!: City[]
--- src/app/components/contract-target-list/contract-target-list.component.ts
+++ src/app/components/contract-target-list/contract-target-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ContractTarget } from 'src/app/models/target.model';
+import { SlideAnimation } from 'src/app/animations.module';
 
 @Component({
 	selector: 'app-contract-target-list',
 	templateUrl: './contract-target-list.component.html',
-	styleUrls: ['./contract-target-list.component.sass']
+	styleUrls: ['./contract-target-list.component.sass'],
+	animations: [SlideAnimation]
 })
 export class ContractTargetListComponent {
 	@Input() targets!: ContractTarget[]
