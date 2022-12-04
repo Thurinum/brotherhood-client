f985039d32c68fd290d7fd6217e710ee5c57ecaf
Thurinum <m.gagnon941@gmail.com>
2022-12-03 20:17:07 -0500

    feat: Add support for status name in helper.httpError

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -4,19 +4,20 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class HelperService {
-	message(msg: string, action: string = "OK") {
-		this.snackBar.open(msg, action, { verticalPosition: "top", panelClass: ["snackbar"] });
-		console.info(msg);
+	message(message: string, action: string = "OK") {
+		this.snackBar.open(message, action, { verticalPosition: "top", panelClass: ["snackbar"] });
+		console.info(message);
 	}
 
-	httpError(msg: string, errorResponse: HttpErrorResponse, action: string = "OK") {
-		const details = errorResponse.error?.errors
+	httpError(attemptedAction: string, errorResponse: HttpErrorResponse, action: string = "OK") {
+		const errorMessage = errorResponse.error?.errors
 			? Object.values(errorResponse.error.errors).join('\n').replace(/,/g, '\n')
 			: errorResponse.error?.message ?? errorResponse.message ?? "Unknown error";
-		const fullMsg = `${msg.toUpperCase()} (${errorResponse.status}):\n\n${details}`;
+		const errorStatus = errorResponse.error.statusName ?? "Form validation error";
+		const fullMessage = `${errorResponse.status} ${errorStatus.toUpperCase()} while ${attemptedAction.toUpperCase()}:\n\n${errorMessage}`;
 
-		this.snackBar.open(fullMsg, action, { verticalPosition: "top", panelClass: ["snackbar", "snackbar-error"] });
-		console.error(fullMsg);
+		this.snackBar.open(fullMessage, action, { verticalPosition: "top", panelClass: ["snackbar", "snackbar-error"] });
+		console.error(fullMessage);
 	}
 
 	constructor(private snackBar: MatSnackBar) { }
